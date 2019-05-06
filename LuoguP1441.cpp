7cd52f8771f53426191485a5510eb2afe30ae6d0
keywet06 <keywet06@tom.com>
2019-05-06 11:19:55 +0800

    commit

--- LuoguP1441.cpp
+++ LuoguP1441.cpp
@@ -1,11 +1,24 @@
 #include <bits/stdc++.h>
 const int N = 25;
-int m, n;
+int ans, m, n;
 int a[N];
+void dfs(int, int);
+void dp();
 int main() {
-    scanf("%d %d", &m, &n);
+    scanf("%d %d", &n, &m);
     for (int i = 1; i <= n; ++i) {
         scanf("%d", &a[i]);
     }
+    dfs(1, 0);
+    printf("%d\n", ans);
     return 0;
+}
+inline void dfs(int x, int y) {
+    if (y > m) {
+        return;
+    }
+    if (x > n) {
+        dp();
+    }
+
 }

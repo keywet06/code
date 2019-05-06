6e5b39746af00a0a04834cab55ecf121525c2217
keywet06 <keywet06@tom.com>
2019-05-06 11:15:54 +0800

    commit

--- LuoguP1441.cpp
+++ LuoguP1441.cpp
@@ -1,7 +1,11 @@
 #include <bits/stdc++.h>
-int a;
+const int N = 25;
+int m, n;
+int a[N];
 int main() {
-    scanf("%d", &a);
-    
+    scanf("%d %d", &m, &n);
+    for (int i = 1; i <= n; ++i) {
+        scanf("%d", &a[i]);
+    }
     return 0;
 }

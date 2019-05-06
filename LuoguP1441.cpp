4636c245adbcf252fdde939d9f9550664fbdf541
keywet06 <keywet06@tom.com>
2019-05-06 13:25:07 +0800

    Update LuoguP1441.cpp

--- LuoguP1441.cpp
+++ LuoguP1441.cpp
@@ -1,7 +1,9 @@
 #include <bits/stdc++.h>
+const int M = 2005;
 const int N = 25;
-int ans, m, n;
-int a[N];
+int ans, m, n, sum, tmp;
+int a[N], b[N];
+int f[M];
 void dfs(int, int);
 void dp();
 int main() {
@@ -14,11 +16,39 @@ int main() {
     return 0;
 }
 inline void dfs(int x, int y) {
+    printf("Debug: dfs(int x = %d, int y = %d);\n", x, y);
     if (y > m) {
         return;
     }
     if (x > n) {
-        dp();
+        if (y == m) {
+            dp();
+        }
+        return;
     }
-
+    b[x] = 1;
+    dfs(x + 1, y + 1);
+    b[x] = 0;
+    dfs(x + 1, y);
+}
+inline void dp() {
+    memset(f, 0, sizeof(f));
+    sum = 0;
+    f[0] = 1;
+    for (int i = 1; i <= n; ++i) {
+        if (b[i]) {
+            continue;
+        }
+        sum += a[i];
+        for (int j = sum; j >= a[i]; --j) {
+            f[j] &= f[j - a[i]];
+        }
+    }
+    tmp = 0;
+    for (int i = 1; i <= sum; ++i) {
+        tmp += f[i];
+    }
+    if (tmp > ans) {
+        ans = tmp;
+    }
 }

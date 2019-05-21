f99416a92893ec1ce7a6f4d52f2a2421af4ff903
keywet06 <42454893+keywet06@users.noreply.github.com>
2019-05-21 22:55:37 +0800

    push

--- JskL1273P104469.cpp
+++ JskL1273P104469.cpp
@@ -1,8 +1,32 @@
 #include <bits/stdc++.h>
-int m, n;
+const int M = 10005;
+const int N = 1005;
+int m, n, sum, x, y;
+int dis[N], head[N];
+int next[M], to[M];
+void addedge(int, int);
+void dfs(int);
 int main() {
     scanf("%d %d", &n, &m);
+    for (int i = 1; i <= m; ++i) {
+        scanf("%d %d", &x, &y);
+        addedge(x, y);
+    }
     for (int i = 1; i <= n; ++i) {
+        for (int j = head[i]; ~j; j = next[j]) {
+            to[j ^ 1] = to[j];
+        }
+        memset(dis, 0, sizeof(dis));
+        sum = 0;
+        for (int j = 1; j <= n; ++j) {
+            if (i == j) {
+                continue;
+            }
+            if (!dis[j]) {
+                ++sum;
+                dfs(j);
+            }
+        }
     }
     return 0;
 }

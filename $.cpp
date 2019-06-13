d4a0698e1bd71fa5cffeb98ffe5000f4a207fe9f
keywet06 <keywet06@tom.com>
2019-06-13 22:08:51 +0800

    push

--- $.cpp
+++ $.cpp
@@ -1,9 +1,10 @@
 #include <bits/stdc++.h>
+#include <conio.h>
 int op(char*, char*);
 int main(int n, char* a[]) {
     int f;
     if (n == 2) {
-        if (op(a[1], "clear")) {
+        if (op(a[1], "-c")) {
             system("del *.in /s");
             system("del *.out /s");
             system("del *.gch /s");
@@ -19,12 +20,47 @@ int main(int n, char* a[]) {
             f = 1;
         }
     } else if (n == 3) {
-        if (op(a[1], "-c")) {
+        if (op(a[1], "-r")) {
             std::string s = "g++ -o ";
             s = s + (std::string)a[2];
             s = s + (std::string)" ";
             s = s + (std::string)a[2];
-            s = s + (std::string)".cpp -lm";
+            s = s + (std::string)".cpp -lm -O3";
+            system(s.data());
+            std::string c;
+            system("chcp > chcp.tmp");
+            freopen("chcp.tmp", "r", stdin);
+            getline(std::cin, c);
+            freopen("con", "r", stdin);
+            system("del chcp.tmp");
+            if (c == (std::string)"活动代码页: 936") {
+                printf("编译完成。\n");
+            } else {
+                printf("Complie is over.\n");
+            }
+            s = (std::string)a[2];
+            system(s.data());
+            if (c == (std::string)"活动代码页: 936") {
+                printf("程序已退出。\n");
+            } else {
+                printf("The program is over.\n");
+            }
+            while (_kbhit()) {
+                getchar();
+            }
+            if (c == (std::string)"活动代码页: 936") {
+                printf("请按任意键继续. . .");
+            } else {
+                printf("Press any key to continue . . .");
+            }
+            _getch();
+            return 0;
+        } else if (op(a[1], "-o")) {
+            std::string s = "g++ -o ";
+            s = s + (std::string)a[2];
+            s = s + (std::string)" ";
+            s = s + (std::string)a[2];
+            s = s + (std::string)".cpp -lm -O3";
             system(s.data());
             return 0;
         }
@@ -41,25 +77,28 @@ int main(int n, char* a[]) {
             puts("");
         }
         puts("kwcmd ———— 一款在windows环境下快速帮助你code的命令行程序");
-        puts("\t版本号：v0.1.0");
+        puts("\t版本号：v0.1.1");
         puts("\t作者：keywet06");
         puts("");
         puts("注意：本程序中 ‘-’ 不与 ‘/’ 、‘\\’ 混用。");
         puts("\t$ -h ———— 帮助信息");
-        puts("\t$ clear ———— 清除本目录下所有多余文件");
-        puts("\t$ -c <C++源文件名> ———— 编译一份C++源代码，其中 <C++源文件名> 不包括 “.cpp”");
+        puts("\t$ -c ———— 清除本目录下所有多余文件");
+        puts("\t$ -o <C++源文件名> ———— 编译一份C++源代码，其中 <C++源文件名> 不包括 “.cpp”");
+        puts("\t$ -r <C++源文件名> ———— 编译并运行一份C++源代码，其中 <C++源文件名> 不包括 “.cpp”");
     } else {
         if (!f) {
             puts("The command syntax is not right.");
             puts("");
         }
         puts("$ ---- A program that can help you to code");
+        puts("\tVersion: v0.1.1");
         puts("\tWriter: keywet06");
         puts("");
         puts("Note: '-' in this program is not mixed with '/' and '\\'.");
         puts("\t$ -h ---- help information");
-        puts("\t$ clear ---- clear all unnecessary files in this directory");
-        puts("\t$ -c <cpp file name> ---- compile a C++ source code where <cpp file name> does not include \".cpp\"");
+        puts("\t$ -c ---- clear all unnecessary files in this directory");
+        puts("\t$ -o <cpp file name> ---- compile a C++ source code where <cpp file name> does not include \".cpp\"");
+        puts("\t$ -c <cpp file name> ---- compile and run a C++ source code where <cpp file name> does not include \".cpp\"");
     }
     return 0;
 }

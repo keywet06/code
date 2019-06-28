7c33dd27e9edf3b87d1acc069a2021d7cf74d343
keywet06 <keywet06@tom.com>
2019-06-28 10:13:51 +0800

    push

---
+++ LuoguP3369.cpp
@@ -0,0 +1,68 @@
+#include <bits/stdc++.h>
+const int N = 100005;
+int cnt, n, num, opt, root, x;
+int father[N], lson[N], rson[N], sum[N], data[N], val[N];
+void insert(int, int = root);
+void pushup(int);
+void updata(int);
+void del(int = root);
+int rank(int = root);
+int prank(int = root);
+int lx(int = root);
+int rx(int = root);
+int main() {
+    srand(time(NULL));
+    scanf("%d", &n);
+    while (n--) {
+        scanf("%d %d", &opt, &x);
+        if (opt == 1) {
+            if (!num) {
+                root = num = sum[cnt] = 1;
+                ++cnt;
+                father[cnt] = 0;
+                data[cnt] = x;
+                val[cnt] = rand();
+            } else {
+                insert(x);
+            }
+        } else if (opt == 2) {
+            del(x);
+        } else if (opt == 3) {
+            printf("%d\n", rank(x));
+        } else if (opt == 4) {
+            printf("%d\n", prank(x));
+        } else if (opt == 5) {
+            printf("%d\n", lx(x));
+        } else if (opt == 6) {
+            printf("%d\n", rx(x));
+        }
+    }
+    return 0;
+}
+void insert(int x, int v) {
+    if (data[v] < x) {
+        if (lson[v]) {
+            insert(x, lson[v]);
+        } else {
+            ++cnt;
+            lson[v] = cnt;
+            data[cnt] = x;
+            sum[cnt] = 1;
+            father[cnt] = v;
+            val[cnt] = rand();
+        }
+    } else {
+        if (rson[v]) {
+            insert(x, rson[v]);
+        } else {
+            ++cnt;
+            rson[v] = cnt;
+            data[cnt] = x;
+            sum[cnt] = 1;
+            father[cnt] = v;
+            val[cnt] = rand();
+        }
+    }
+    updata(v);
+    pushup(v);
+}

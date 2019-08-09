16fee2e18247d64d61846753d112d84d04d4dc09
keywet06 <42454893+keywet06@users.noreply.github.com>
2019-08-09 19:20:57 +0800

    push

--- LuoguP4555.cpp
+++ LuoguP4555.cpp
@@ -1,17 +1,5 @@
 #include <bits/stdc++.h>
-namespace kw {
-    template <typename T> T& tomin(T&, T);
-    template <typename T> T& tomax(T&, T);
-}
-namespace kw {
-    template <typename T> inline T& tomin(T& x, T y) {
-        return x = std::min(x, y);
-    }
-    template <typename T> inline T& tomax(T& x, T y) {
-        return x = std::max(x, y);
-    }
-}
-const int N = 2e5 + 5;
+const int N = 3e5 + 5;
 char t[N], s[N];
 int ans, n, now, r;
 int al[N], ar[N], d[N];
@@ -37,18 +25,18 @@ int main() {
             r = i + d[i];
             now = i;
         }
-        kw::tomax(ar[i - d[i]], d[i]);
-        kw::tomax(al[i + d[i]], d[i]);
+        ar[i - d[i]] = std::max(ar[i - d[i]], d[i]);
+        al[i + d[i]] = std::max(al[i + d[i]], d[i]);
     }
     for (int i = 2; i <= n; ++i) {
-        kw::tomax(ar[i], ar[i - 1] - 1);
+        ar[i] = std::max(ar[i], ar[i - 1] - 1);
     }
     for (int i = n - 1; i; --i) {
-        kw::tomax(al[i], al[i + 1] - 1);
+        al[i] = std::max(al[i], al[i + 1] - 1);
     }
-    for (int i = 1; i <= n; ++i) {
-        if (ar[i] && al[i]) {
-            kw::tomax(ans, ar[i] + al[i] + 1);
+    for (int i = 1; i < n; ++i) {
+        if ((al[i] + 1 - i % 2) && (ar[i + 1] + i % 2)) {
+            ans = std::max(ans, al[i] + ar[i + 1] + 1);
         }
     }
     printf("%d\n", ans);

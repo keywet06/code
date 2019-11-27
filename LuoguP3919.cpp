1c1971c97c73189c08859a5c223a7594d08c1d69
keywet06 <42454893+keywet06@users.noreply.github.com>
2019-11-27 23:22:44 +0800

    push

---
+++ LuoguP3919.cpp
@@ -0,0 +1,25 @@
+    #include "PersistentArray.hpp"
+const int N = 1000005;
+Octinc::PersistentArray<int, N> a;
+int m, n, x, y, z;
+int version[N];
+int main() {
+    scanf("%d %d", &n, &m);
+    for (int i = 1; i <= n; ++i) {
+        scanf("%d", &a[i]);
+    }
+    version[0] = a.getVersion();
+    for (int i = 1; i <= m; ++i) {
+        scanf("%d %d", &z, &x);
+        z = version[z];
+        if (x == 1) {
+            scanf("%d %d", &x, &y);
+            a(z, x) = y;
+        } else {
+            scanf("%d", &x);
+            printf("%d\n", a(z, x));
+        }
+        version[i] = a.getVersion();
+    }
+    return 0;
+}
---
+++ PersistentArray.hpp
@@ -0,0 +1,110 @@
+#ifndef OCTINC_PERSISTENTARRAY_HPP
+#  define OCTINC_PERSISTENTARRAY_HPP
+#  ifndef BITS_STDCPP_H
+#    define BITS_STDCPP_H
+#    include <bits/stdc++.h>
+#  endif
+#  if __cplusplus < 201103L && !defined(nullptr)
+#    define nullptr NULL
+#  endif
+namespace Octinc {
+template <typename ValueType, size_t length>
+  class PersistentArray {
+   protected:
+    class Node {
+       public:
+        Node *lson, *rson;
+        ValueType value;
+        Node();
+    };
+    std::vector<Node *> version;
+    Node *NewVersion();
+    Node *NewVersion(size_t);
+    Node *NewNode(Node *&);
+    ValueType &query(Node *, size_t, size_t, size_t);
+   public:
+    PersistentArray();
+    ~PersistentArray();
+    void clear();
+    ValueType &operator[](size_t);
+    ValueType &operator()(size_t, size_t);
+    size_t getVersion();
+};
+} // namespace Octinc
+namespace Octinc {
+template <typename ValueType, size_t length>
+  inline PersistentArray<ValueType, length>::Node::Node() {
+    this->lson = this->rson = nullptr;
+}
+template <typename ValueType, size_t length>
+  inline typename PersistentArray<ValueType, length>::Node
+  *PersistentArray<ValueType, length>::NewVersion() {
+    this->version.push_back(new Node);
+    return &(*(this->version[this->version.size() - 1]) =
+      *(this->version[this->version.size() - 2]));
+}
+template <typename ValueType, size_t length>
+  inline typename PersistentArray<ValueType, length>::Node
+  *PersistentArray<ValueType, length>::NewVersion(size_t v) {
+    this->version.push_back(new Node);
+    return &(*this->version[this->version.size() - 1] = *this->version[v]);
+}
+template <typename ValueType, size_t length>
+  inline typename PersistentArray<ValueType, length>::Node
+  *PersistentArray<ValueType, length>::NewNode(Node *&v) {
+    return v ? v = &(*(new Node) = *v) : v = new Node;
+}
+template <typename ValueType, size_t length>
+  inline ValueType &PersistentArray<ValueType, length>::query
+  (Node *v, size_t x, size_t l, size_t r) {
+    return l == r ? v->value : (x <= l + r >> 1 ?
+      query(this->NewNode(v->lson), x, l, l + r >> 1) :
+      query(this->NewNode(v->rson), x, (l + r >> 1) + 1, r));
+}
+template <typename ValueType, size_t length>
+  inline PersistentArray<ValueType, length>::PersistentArray() {
+    version.push_back(new Node);
+}
+template <typename ValueType, size_t length>
+  inline PersistentArray<ValueType, length>::~PersistentArray() {
+    this->clear();
+}
+template <typename ValueType, size_t length>
+  inline void PersistentArray<ValueType, length>::clear() {
+    std::set<Node *> set;
+    std::queue<Node *> queue;
+    for (size_t i = this->version.size() - 1; ~i; --i) {
+        set.insert(this->version[i]);
+        queue.push(this->version[i]);
+    }
+    this->version.clear();
+    while (!queue.empty()) {
+        Node *v = queue.front();
+        queue.pop();
+        if (!set.count(v->lson)) {
+            set.insert(v->lson);
+            queue.push(v->lson);
+        }
+        if (!set.count(v->rson)) {
+            set.insert(v->rson);
+            queue.push(v->rson);
+        }
+        delete v;
+    }
+}
+template <typename ValueType, size_t length>
+  inline ValueType &PersistentArray<ValueType, length>::operator[](size_t x) {
+    ValueType &tmp = this->query(this->NewVersion(), x, 0, length - 1);
+    return tmp;
+}
+template <typename ValueType, size_t length>
+  inline ValueType &PersistentArray<ValueType, length>::operator()(size_t x, size_t y) {
+    ValueType &tmp = this->query(this->NewVersion(x), y, 0, length - 1);
+    return tmp;
+}
+template <typename ValueType, size_t length>
+  inline size_t PersistentArray<ValueType, length>::getVersion() {
+    return this->version.size() - 1;
+}
+} // namespace Octinc
+#endif

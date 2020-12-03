b440137b8408beb17b6d09a9cf8985975c7d2ba4
keywet06 <keywet06@tom.com>
2020-12-03 22:45:38 +0800

    add

---
+++ .oct/needcpp11.cpp
@@ -0,0 +1,9 @@
+// oct code object needcpp11.cpp
+#ifndef OCT_CODE_OBJECT_NEEDCPP11x2eCPP
+#define OCT_CODE_OBJECT_NEEDCPP11x2eCPP
+
+#if __cplusplus < 201103L
+#error "This program or object needs C++11 or higher standard"
+#endif
+
+#endif // oct code end needcpp11.cpp

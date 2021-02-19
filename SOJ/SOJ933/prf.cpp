6d853c08d220d8009f3855f40364297424f2bb93
keywet06 <keywet06@tom.com>
2021-02-19 13:53:57 +0800

    SOJ Round #15

---
+++ SOJ/SOJ933/prf.cpp
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+
+int main() {
+    for (int i = 1; i <= 50; ++i) {
+        std::cout << "./fit < couple" << i << ".txt > couple" << i << ".out" << std::endl;
+    }
+    return 0;
+}

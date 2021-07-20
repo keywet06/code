5cec7c62517ab6139c7833fd29c6581392dff716
keywet06 <keywet06@tom.com>
2021-07-20 23:55:24 +0800

    unfinished LuoguP3690

--- Luogu/LuoguP4254.cpp
+++ Luogu/LuoguP4254.cpp
@@ -4,17 +4,17 @@ using dou = double;
 
 const int N = 100005;
 const int Ns = 50000;
-const dou exp = 1e-5;
 
 int n, q, cnt;
 int best[N];
 
+dou x;
 dou k[N], b[N];
 
 std::string s;
 
 inline dou cross(int x, int y) {
-    if (std::abs(k[x] - k[y]) < exp) return -1;
+    if (std::abs(k[x] - k[y])) return -1;
     return -(b[x] - b[y]) / (k[x] - k[y]);
 }
 
@@ -30,22 +30,24 @@ inline void update(int u, int l, int r, int id) {
     dou c = cross(id, p);
     if (f(id, mid) > f(p, mid)) {
         best[u] = id;
-        if (c >= l - exp && c <= mid + exp) update(u << 1, l, mid, p);
-        if (c > mid && c <= r + exp) update(u << 1 | 1, mid + 1, r, p);
+        if (c >= l && c <= mid) update(u << 1, l, mid, p);
+        if (c > mid && c <= r) update(u << 1 | 1, mid + 1, r, p);
     } else {
-        if (c >= l - exp && c <= mid + exp) update(u << 1, l, mid, id);
-        if (c > mid && c <= r + exp) update(u << 1 | 1, mid + 1, r, id);
+        if (c >= l && c <= mid) update(u << 1, l, mid, id);
+        if (c > mid && c <= r) update(u << 1 | 1, mid + 1, r, id);
     }
 }
 
 inline dou query(int u, int l, int r, int x) {
     if (l == r) return f(best[u], l);
-
+    int mid = l + r >> 1;
+    return std::max(f(best[u], x), x <= mid ? query(u << 1, l, mid, x)
+                                            : query(u << 1 | 1, mid + 1, r, x));
 }
 
 int main() {
-    std::ios::sync_with_stdio(0);
-    std::cin.tie(0), std::cout.tie(0);
+    // std::ios::sync_with_stdio(0);
+    // std::cin.tie(0), std::cout.tie(0);
     std::cin >> n;
     for (int i = 1; i <= n; ++i) {
         std::cin >> s;
@@ -53,6 +55,10 @@ int main() {
             ++cnt;
             std::cin >> b[cnt] >> k[cnt];
             update(1, 1, Ns, cnt);
+        } else if (s == "Query") {
+            std::cin >> x;
+            std::cerr << "(" << query(1, 1, n, x) << "): ";
+            std::cout << int(floor(query(1, 1, n, x) + 1e-4) / 100) << '\n';
         }
     }
     return 0;

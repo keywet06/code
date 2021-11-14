2882604c4fe3999f01987018545f4d3496f084f2
keywet06 <keywet06@tom.com>
2021-11-14 19:16:47 +0800

    accept Zhengrui2089

--- Zhengrui/Zhengrui2089.cpp
+++ Zhengrui/Zhengrui2089.cpp
@@ -1,57 +1,56 @@
 #include <bits/stdc++.h>
 
-const int N = 1 << 20 | 5;
-const int M = 1 << 21 | 5;
+const int C = 20;
+const int E = 1 << C;
+const int N = 1 << C + 1;
+const int R = E - 1;
 
-int q, flag, o, x, num, ret, cnt, c1, c2, c3;
-int po[N], px[N], is[N], vis[N], a1[N], a2[N], a3[N], a[N];
-int size[M];
+struct trie : public std::bitset<N> {
+    std::bitset<N> &th = *this;
+
+    void insert(int x) {
+        x |= E;
+        while (x) th[x] = 1, x >>= 1;
+    }
+
+    void insertOr(int x) {
+        insert(x);
+        for (int i = 1; i < E; i <<= 1) {
+            if (x & i && !th[E | x ^ i]) insertOr(x ^ i);
+        }
+    }
+
+    int getXor(int x) {
+        int ret = 0, u = 1, t;
+        for (int i = 1 << C - 1; i; i >>= 1) {
+            if (t = th[u << 1 | !(x & i)]) ret |= i;
+            u = u << 1 | t ^ bool(x & i);
+        }
+        return ret;
+    }
+
+    int getOr(int x) { return getXor(x); }
+
+    int getAnd(int x) { return getOr(R ^ x) ^ R ^ x; }
+};
+
+int q, o, x;
+
+trie txor, tor;
 
 int main() {
     std::ios::sync_with_stdio(0);
     std::cin.tie(0), std::cout.tie(0);
     std::cin >> q;
-    for (int i = 1; i <= q; ++i) std::cin >> po[i] >> px[i], flag |= po[i] == 2;
-    if (flag) {
-        for (int _ = 1; o = po[_], x = px[_], _ <= q; ++_) {
-            if (o == 1) {
-                if (is[x]) continue;
-                a[++cnt] = x;
-            } else {
-                if (vis[x] != cnt) {
-                    vis[x] = cnt, c1 = c2 = c3 = 0;
-                    for (int i = 1; i <= cnt; ++i) {
-                        c1 = std::max(c1, x ^ a[i]);
-                        c2 = std::max(c2, x & a[i]);
-                        c3 = std::max(c3, x | a[i]);
-                    }
-                    a1[x] = c1, a2[x] = c2, a3[x] = c3;
-                }
-                std::cout << a1[x];
-                if (o == 2) std::cout << ' ' << a2[x] << ' ' << a3[x];
-                std::cout << '\n';
-            }
-        }
-    } else {
-        for (int _ = 1; o = po[_], x = px[_], _ <= q; ++_) {
-            if (o == 1) {
-                ++size[c1 = 1];
-                for (int i = 19; i >= 0; --i) {
-                    ++size[c1 = c1 << 1 | bool(x & (1 << i))];
-                }
-            } else {
-                c1 = 1, c3 = 0;
-                for (int i = 19; i >= 0; --i) {
-                    c2 = bool(x & (1 << i));
-                    if (size[c1 << 1 | !c2]) {
-                        c1 = c1 << 1 | !c2;
-                        c3 |= 1 << i;
-                    } else {
-                        c1 = c1 << 1 | c2;
-                    }
-                }
-                std::cout << c3 << '\n';
-            }
+    while (q--) {
+        std::cin >> o >> x;
+        if (o == 1) {
+            txor.insert(x), tor.insertOr(x);
+        } else if (o == 2) {
+            std::cout << txor.getXor(x) << ' ' << tor.getAnd(x) << ' '
+                      << tor.getOr(x) << '\n';
+        } else {
+            std::cout << txor.getXor(x) << '\n';
         }
     }
     return 0;

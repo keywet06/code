a1f6ee1b10375fe9077559c7b2d57db69a958a44
keywet06 <keywet06@tom.com>
2021-12-09 12:10:39 +0800

    accept CF526F

--- CF/CF526F.cpp
+++ CF/CF526F.cpp
@@ -1,76 +1,97 @@
 #include <bits/stdc++.h>
 
+using namespace std;
+
 using int64 = long long;
 
-const int N = 300005;
-const int M = N << 1;
+const int N = 500005;
 
-int n, x, y, z, s, t;
-int a[N], max[N], min[N];
-int c[M], d[M];
+int n, top1, top2;
 
-bool p(int x, int l, int r) { return x >= l && x <= r; }
+int64 ans;
 
-void solve(int l, int r) {
-    // std::cerr << "solve(" << l << ", " << r << "): (";
-    int temp = s;
-    if (l == r) return /* std::cerr << 1 << ")" << std::endl, */ void(++s);
-    int m = l + r >> 1;
-    max[m] = min[m] = a[m];
-    for (int i = m - 1; i >= l; --i) {
-        max[i] = std::max(a[i], max[i + 1]);
-        min[i] = std::min(a[i], min[i + 1]);
+struct chess {
+    int x, y;
+    bool operator<(const chess &P) const { return x < P.x; }
+} c[N], stk1[N], stk2[N];
+
+struct node {
+    int Min, tag;
+    int64 cnt;
+} t[N << 2];
+
+void build(int i, int l, int r) {
+    if (l == r) {
+        t[i].Min = l;
+        t[i].cnt = 1;
+        return;
     }
-    max[m + 1] = min[m + 1] = a[m + 1];
-    for (int i = m + 2; i <= r; ++i) {
-        max[i] = std::max(a[i], max[i - 1]);
-        min[i] = std::min(a[i], min[i - 1]);
+    int mid = (l + r) >> 1;
+    build(i << 1, l, mid);
+    build(i << 1 | 1, mid + 1, r);
+    t[i] = t[i << 1];
+}
+
+void pushdown(int i) {
+    int ls = i << 1, rs = ls | 1;
+    t[ls].Min += t[i].tag;
+    t[rs].Min += t[i].tag;
+    t[ls].tag += t[i].tag;
+    t[rs].tag += t[i].tag;
+    t[i].tag = 0;
+}
+
+void pushup(int i) {
+    int ls = i << 1, rs = ls | 1;
+    t[i].Min = min(t[ls].Min, t[rs].Min);
+    t[i].cnt = 0;
+    if (t[i].Min == t[ls].Min) t[i].cnt += t[ls].cnt;
+    if (t[i].Min == t[rs].Min) t[i].cnt += t[rs].cnt;
+}
+
+void update(int i, int l, int r, int x, int y, int val) {
+    if (l >= x && r <= y) {
+        t[i].Min += val;
+        t[i].tag += val;
+        return;
     }
-    for (int i = 1; i <= m; ++i) {
-        x = max[i] - min[i];
-        s += p(i + x, m + 1, r) && max[i + x] <= max[i] && min[i + x] >= min[i];
-    }
-    // std::cerr << s - temp << ", ", temp = s;
-    for (int i = m + 1; i <= r; ++i) {
-        x = max[i] - min[i];
-        s += p(i - x, l, m) && max[i - x] <= max[i] && min[i - x] >= min[i];
-    }
-    // std::cerr << s - temp << ", ", temp = s;
-    ++t, x = y = m;
-    for (int i = m + 1; i <= r; ++i) {
-        while (x >= l && max[x] < max[i]) {
-            if (d[z = x - min[x] + N] < t) d[z] = t, c[z] = 0;
-            ++c[z], --x;
-        }
-        while (y >= x && min[y] > min[i]) --c[y - min[y] + N], --y;
-        s += d[z = i - max[i] + N] == t ? c[z] : 0;
-    }
-    // std::cerr << s - temp << ", ", temp = s;
-    ++t, x = y = m;
-    for (int i = m + 1; i <= r; ++i) {
-        while (x >= l && min[x] > min[i]) {
-            if (d[z = x + max[x] + N] < t) d[z] = t, c[z] = 0;
-            ++c[z], --x;
-            // std::cout << "(" << z - N << ", ";
-        }
-        while (y >= x && max[y] < max[i]) --c[y + max[y] + N], --y;
-        s += d[z = i + min[i] + N] == t ? c[z] : 0;
-        // std::cerr << (i + min[i]) << ")";
-    } 
-    // std::cerr << s - temp << ")" << std::endl;
-    // for (int i = l; i <= r; ++i) std::cerr << min[i] << ' ';
-    // std::cerr << std::endl;
-    // for (int i = l; i <= r; ++i) std::cerr << max[i] << ' ';
-    // std::cerr << std::endl;
-    solve(l, m), solve(m + 1, r);
+    int mid = (l + r) >> 1;
+    pushdown(i);
+    if (mid >= x) update(i << 1, l, mid, x, y, val);
+    if (mid < y) update(i << 1 | 1, mid + 1, r, x, y, val);
+    pushup(i);
+}
+
+int query(int i, int l, int r, int x, int y) {
+    if (l >= x && r <= y) return t[i].Min == 0 ? t[i].cnt : 0;
+    int mid = (l + r) >> 1, ans = 0;
+    pushdown(i);
+    if (mid >= x) ans += query(i << 1, l, mid, x, y);
+    if (mid < y) ans += query(i << 1 | 1, mid + 1, r, x, y);
+    return ans;
 }
 
 int main() {
-    std::ios::sync_with_stdio(0);
-    std::cin.tie(0), std::cout.tie(0);
-    std::cin >> n;
-    for (int i = 1; i <= n; ++i) std::cin >> x >> y, a[x] = y;
-    solve(1, n);
-    std::cout << s << std::endl;
+    scanf("%d", &n);
+    for (int i = 1; i <= n; ++i) scanf("%d%d", &c[i].x, &c[i].y);
+    sort(c + 1, c + n + 1);
+    build(1, 1, n);
+    for (int i = 1; i <= n; ++i) {
+        update(1, 1, n, 1, n, -1);
+        while (top1 && stk1[top1].y < c[i].y) {
+            update(1, 1, n, stk1[top1 - 1].x + 1, stk1[top1].x,
+                   c[i].y - stk1[top1].y),
+                --top1;
+        }
+        stk1[++top1] = {i, c[i].y};
+        while (top2 && stk2[top2].y > c[i].y) {
+            update(1, 1, n, stk2[top2 - 1].x + 1, stk2[top2].x,
+                   stk2[top2].y - c[i].y),
+                --top2;
+        }
+        stk2[++top2] = {i, c[i].y};
+        ans += query(1, 1, n, 1, i);
+    }
+    printf("%lld\n", ans);
     return 0;
 }

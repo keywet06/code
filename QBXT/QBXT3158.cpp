42df37971e074c02b64cbe704acffa3244197865
keywet06 <keywet06@tom.com>
2021-12-11 15:48:01 +0800

    accept QBXT3158

---
+++ QBXT/QBXT3158.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using int64 = long long;
+
+template <typename Type>
+inline Type GetPrimitiveRoot(Type n) {
+    
+}
+
+int main() {
+    std::ios::sync_with_stdio(0);
+    std::cin.tie(0), std::cout.tie(0);
+    return 0;
+}

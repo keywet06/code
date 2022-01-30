e9ab9bcc8e54bf3459f5ec6f28d5fc66ad4e9942
keywet06 <keywet06@tom.com>
2022-01-30 15:12:30 +0800

    Move and Update compare.sh

---
+++ compare.sh
@@ -0,0 +1,26 @@
+if g++ -o $1 $1.cpp -std=c++14 &&
+   g++ -o $2 $2.cpp -std=c++14 &&
+   g++ -o $3 $3.cpp -std=c++14; then
+
+    echo "Compile success"
+
+    while true; do
+
+        ./$3 > $1.in
+        ./$2 < $1.in > $1.ans
+        ./$1 < $1.in > $1.out
+
+        if diff $1.in $1.ans; then
+            echo "Success";
+        else
+            echo "Failed";
+            break;
+        fi
+
+    done
+
+else
+
+    echo "Compile Failed"
+
+fi

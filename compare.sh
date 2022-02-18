37786040f36cc78fcc62f3fb3f4aedb413754ec9
keywet06 <keywet06@tom.com>
2022-02-18 22:35:45 +0800

    fix error

--- compare.sh
+++ compare.sh
@@ -1,16 +1,16 @@
-if g++ -o $1 $1.cpp -std=c++14 &&
-   g++ -o $2 $2.cpp -std=c++14 &&
-   g++ -o $3 $3.cpp -std=c++14; then
+if g++ -o $1 $1.cpp -std=c++14 -O2 &&
+   g++ -o $2 $2.cpp -std=c++14 -O2 &&
+   g++ -o $3 $3.cpp -std=c++14 -O2; then
 
     echo "Compile success"
 
     while true; do
 
-        ./$3 > $1.in
-        ./$2 < $1.in > $1.ans
-        ./$1 < $1.in > $1.out
+        time ./$3 > $1.in
+        time ./$2 < $1.in > $1.ans
+        time ./$1 < $1.in > $1.out
 
-        if diff $1.in $1.ans; then
+        if diff $1.out $1.ans; then
             echo "Success";
         else
             echo "Failed";

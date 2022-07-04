fd3619dffa41996bd3a74985bd505432af7604db
keywet06 <keywet06@tom.com>
2022-07-04 23:24:37 +0800

    finish struct Solver

--- UOJ/UOJ55.cpp
+++ UOJ/UOJ55.cpp
@@ -32,6 +32,7 @@ class Pool {
 
     inline size_t Now() { return Top; }
     inline size_t New() { return ++Top; }
+    inline void Del(Type ClearData = Type()) { Data[Top--] = ClearData; }
 
     inline Type &operator[](size_t i) { return Data[i]; }
 };
@@ -42,23 +43,22 @@ const int Z = 100;  // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
 struct SplayNode;
 
-Oct::Pool<SplayNode, Z> Tr;
-
-struct SplayNodePtr {
-    int i;
-    inline SplayNodePtr(int ci = 0) : i(ci) {}
-    inline SplayNodePtr(SplayNode *cPtr) : i(cPtr - &Tr[0]) {}
-    inline operator bool() { return bool(i); }
-    inline SplayNode *operator->() { return &Tr[i]; }
-    inline SplayNode *Ptr() { return &Tr[i]; }
-};
-
-inline bool operator==(SplayNodePtr a, SplayNodePtr b) { return a.i == b.i; }
+extern Oct::Pool<SplayNode, Z> Tr;
 
 struct SplayNode {
-    SplayNodePtr Fa, Son[2];
-    int Sum;
-    inline SplayNode(int cFa = 0, int cSonl = 0, int cSonr = 0, int cSum = 1)
+    struct Ptr {
+        int i;
+        inline Ptr(int ci = 0) : i(ci) {}
+        inline Ptr(SplayNode *cPtr) : i(cPtr - &Tr[0]) {}
+        inline operator bool() { return bool(i); }
+        inline SplayNode *operator->() { return &Tr[i]; }
+        inline explicit operator SplayNode *() { return &Tr[i]; }
+        inline bool operator==(Ptr b) { return i == b.i; }
+        inline SplayNode &operator&() { return Tr[i]; }
+    };
+    Ptr Fa, Son[2];
+    int Sum, Val;
+    inline SplayNode(int cVal = 0, int cFa = 0, int cSonl = 0, int cSonr = 0, int cSum = 1)
         : Fa(cFa), Son({cSonl, cSonr}), Sum(cSum) {}
     inline void PushUp() { Sum = Son[0]->Sum + Son[1]->Sum + 1; }
     inline int GetDir() { return Fa->Son[1] == this; }
@@ -67,18 +67,46 @@ struct SplayNode {
         if (Fa->Son[c] = Son[d]) Son[d]->Fa = Fa;
         if (Fa = (Son[d] = Fa)->Fa) Fa->Son[p] = this;
         Son[d]->Fa = this;
+        Son[d]->PushUp(), PushUp();
     }
     inline void Splay() {
-        
+        PushUp();
+        for (; Fa; Rotate()) {
+            if (Fa->Fa) GetDir() == Fa->GetDir() ? Fa->Rotate() : Rotate();
+        }
     }
 };
 
-inline SplayNodePtr Tree(int u) { return u; }
+Oct::Pool<SplayNode, Z> Tr;
 
 struct Solver {
-    SplayNodePtr Root;
-
-    inline int GetDir(SplayNodePtr u) { return u->Fa->Son[1] == u; }
+    SplayNode::Ptr Root;
+    inline void Splay(SplayNode::Ptr u) { u->Splay(), Root = u; }
+    inline SplayNode::Ptr InsertNode(int x) {
+        SplayNode::Ptr u = Tr.New();
+        &u = SplayNode(x);
+        if (!Root) return Root = u;
+        SplayNode::Ptr c = Root;
+        while (true) {
+            int Dir = x > c->Val;
+            if (c->Son[Dir]) {
+                c = c->Son[Dir];
+            } else {
+                c->Son[Dir] = u;
+                break;
+            }
+        }
+        return Splay(u), u;
+    }
+    inline int Insert(int x) {
+        auto u = InsertNode(-x);
+        int Ans = u->Son[1]->Sum + 1;
+        if (!u->Son[0]) return (Root = u->Son[1])->Fa = 0, Tr.Del(), Ans;
+        for (u = u->Son[0]; u->Son[1];) u = u->Son[1];
+        u->Son[1] = Root->Son[1], (Root = Root->Son[0])->Fa = 0;
+        if (u->Son[1]) u->Son[1]->Fa = u, Splay(u->Son[1]);
+        return Splay(u->Son[1]), Tr.Del(), Ans;
+    }
 };
 
 int main() {

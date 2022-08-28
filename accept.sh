08884e10bfa5182cdc54a4bd07b3a290318e2783
keywet06 <keywet06@tom.com>
2022-08-28 13:09:33 +0800

    Update accept.sh

--- accept.sh
+++ accept.sh
@@ -1,2 +1,17 @@
-git add $1/$1$2.cpp
-git commit -m"accept ${1}${2}"
+cd "$( dirname "${BASH_SOURCE[0]}" )"
+
+if [[ -f "$1/$1$2.cpp" ]]; then
+    ADDFILE="$1/$1$2.cpp";
+    ADDMESSAGE="$1$2";
+elif [[ -f "$1/$2.cpp" ]]; then
+    ADDFILE="$1/$2.cpp";
+    ADDMESSAGE="$2";
+else
+    echo "FILE is not exist";
+    exit 1;
+fi
+
+git add $ADDFILE
+git commit $ADDFILE -m"accept $ADDMESSAGE"
+
+exit 0;
